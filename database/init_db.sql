ee98960d5bf6e38f82a91c832899ad76a7e7e320
bmk <ibrahim.kakaev@gmail.com>
2024-05-03 13:33:18 -0400

    Fonctionnaliter toute faites, manque l'inscription et le placment de la carte et des statistique

--- database/init_db.sql
+++ database/init_db.sql
@@ -1,8 +1,6 @@
 -- Créer la base de données
 DROP DATABASE IF EXISTS outils_db;
 CREATE DATABASE IF NOT EXISTS outils_db;
-
--- Utiliser la base de données créée
 USE outils_db;
 
 -- Créer la table 'users'
@@ -11,35 +9,121 @@ CREATE TABLE IF NOT EXISTS users (
     nom VARCHAR(100),
     prenom VARCHAR(100),
     email VARCHAR(100),
+    pp MEDIUMBLOB, -- photo de profil
     password VARCHAR(100),
     date_enter TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Créer la table 'annonces'
+-- Créer les autres tables nécessaires
+CREATE TABLE IF NOT EXISTS conversations (
+    conversation_id INT AUTO_INCREMENT PRIMARY KEY,
+    creator_idname VARCHAR(10),
+    receiver_idname VARCHAR(10),
+    title VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS messages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    conversation_id INT,
+    user_idname VARCHAR(10),
+    user_dest_idname VARCHAR(10),
+    message TEXT NOT NULL,
+    sent_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (conversation_id) REFERENCES conversations(conversation_id) ON DELETE CASCADE,
+    FOREIGN KEY (user_idname) REFERENCES users(idname) ON DELETE CASCADE
+);
+
 CREATE TABLE IF NOT EXISTS annonces (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     content TEXT NOT NULL,
-    price DECIMAL(10,2) NOT NULL, -- Spécifiez la précision pour le prix
+    price DECIMAL(10,2) NOT NULL,
     category VARCHAR(255) NOT NULL,
+    sous_category VARCHAR(255) NOT NULL,
     city VARCHAR(255) NOT NULL,
     postal_code VARCHAR(255) NOT NULL,
     author_idname VARCHAR(100),
+    author_pp VARCHAR(255),
     published_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    image MEDIUMBLOB,  -- Modifier pour stocker le chemin de l'image
-    FOREIGN KEY (author_idname) REFERENCES users(idname) ON DELETE SET NULL -- Ajustement pour gérer la suppression
+    image VARCHAR(255),
+    FOREIGN KEY (author_idname) REFERENCES users(idname) ON DELETE SET NULL
 );
 
--- Insérer des données dans 'users'
+
 INSERT INTO users (idname, nom, prenom, email, password) VALUES 
 ('notorious', 'McGregor', 'Conor', 'mcgregor.conor@gmail.com', 'password123'),
 ('putin', 'Khabib', 'Nurmagomedov', 'nurma.khabib@gmail.com', 'password123'),
-('jon.jones', 'Jones', 'Jon', 'jonnybones@gmail.com', 'password123');
+('jon.jones', 'Jones', 'Jon', 'jonnybones@gmail.com', 'password123'),
+('rocky', 'Balboa', 'Rocky', 'rrr@gmail.com', 'password'),
+('creed', 'Creed', 'Adonis', 'creed.adonis@gmail.com', 'password123'),
+('apollo', 'Creed', 'Apollo', 'apollo.creed@gmail.com', 'password123'),
+('drago', 'Drago', 'Ivan', 'drago.ivan@gmail.com', 'password123');
 
--- Insérer des données dans 'annonces'
-INSERT INTO annonces (title, content, price, category, city, postal_code, author_idname) VALUES 
-('Scie circulaire', 'Scie circulaire Bosch 1400W', 99.99, 'Outils', 'Paris', '75000', 'notorious'),
-('Marteau', 'Marteau 500g', 9.99, 'Outils', 'Marseille', '13000', 'putin'),
-('Perceuse', 'Perceuse sans fil 18V', 129.99, 'Outils', 'Lyon', '69000', 'jon.jones'),
-('Ponceuse', 'Ponceuse excentrique 300W', 49.99, 'Outils', 'Toulouse', '31000', 'notorious'),
-('Tournevis', 'Tournevis cruciforme', 4.99, 'Outils', 'Bordeaux', '33000', 'putin');
+-- Insérer les conversations
+INSERT INTO conversations  (creator_idname, receiver_idname, title) VALUES 
+('notorious', 'putin', 'Conor vs Khabib'),
+('notorious', 'jon.jones', 'Conor vs Jon'),
+('putin', 'jon.jones', 'Khabib vs Jon');
+
+
+-- Maintenant, insérer les messages en utilisant les IDs corrects
+INSERT INTO messages (conversation_id, user_idname, user_dest_idname, message) VALUES 
+(1, 'notorious', 'putin', 'Salut Khabib, tu vas bien ?'),
+(1, 'putin', 'notorious', 'Salut Conor, oui et toi ?'),
+(2, 'notorious', 'jon.jones', 'Salut Jon, tu vas bien ?'),
+(2, 'jon.jones', 'notorious', 'Salut Conor, oui et toi ?'),
+(3, 'putin', 'jon.jones', 'Salut Jon, tu vas bien ?'),
+(3, 'jon.jones', 'putin', 'Salut Khabib, oui et toi ?');
+
+
+-- Continuation des insertions dans 'annonces' avec ajustement des prix
+INSERT INTO annonces (title, content, price, category, sous_category, city, postal_code, author_idname, image) VALUES 
+('Échelle télescopique', 'Échelle pliable en aluminium', 24, 'Maison & Jardin', 'Échelles et échafaudages', 'Nice', '06000', 'rocky', '/home/bmk/Documents/techno4/PretOutils/database/echelle.jpg'),
+('Tondeuse à gazon', 'Tondeuse électrique 1600W', 40, 'Maison & Jardin', 'Entretien du gazon', 'Nantes', '44000', 'creed', '/home/bmk/Documents/techno4/PretOutils/database/tondeuse.jpg'),
+('Tronçonneuse', 'Tronçonneuse thermique 45cm', 52, 'Maison & Jardin', 'Outils de taille', 'Strasbourg', '67000', 'rocky', '/home/bmk/Documents/techno4/PretOutils/database/tronconneuse.jpg'),
+('Kit d irrigation', 'Kit complet pour arrosage de jardin', 18, 'Maison & Jardin', 'Outils de plantation', 'Montpellier', '34000', 'apollo', '/home/bmk/Documents/techno4/PretOutils/database/irrigation.jpg'),
+('Kit de peinture', 'Rouleaux, pinceaux et bâche', 6, 'Maison & Jardin', 'Outils de peinture', 'Lille', '59000', 'drago', '/home/bmk/Documents/techno4/PretOutils/database/peinture.jpg'),
+('Souffleuse à feuilles', 'Souffleuse électrique 3000W', 30, 'Maison & Jardin', 'Entretien du gazon', 'Rennes', '35000', 'rocky', '/home/bmk/Documents/techno4/PretOutils/database/souffleuse.jpg'),
+('Rouleau à gazon', 'Rouleau manuel pour aplatir le gazon', 15, 'Maison & Jardin', 'Entretien du gazon', 'Reims', '51100', 'creed', '/home/bmk/Documents/techno4/PretOutils/database/rouleau.jpg'),
+('Étagère de garage', 'Étagère métallique robuste', 20, 'Maison & Jardin', 'Échelles et échafaudages', 'Saint-Étienne', '42000', 'apollo', '/home/bmk/Documents/techno4/PretOutils/database/etagere.jpg'),
+('Compresseur d air', 'Compresseur 50L', 50, 'Maison & Jardin', 'Outils électriques', 'Le Havre', '76600', 'drago', '/home/bmk/Documents/techno4/PretOutils/database/compresseur.jpg'),
+('Niveau laser', 'Niveau laser automatique', 16, 'Maison & Jardin', 'Mesure et traçage', 'Toulon', '83000', 'rocky', '/home/bmk/Documents/techno4/PretOutils/database/niveau.jpg');
+
+
+INSERT INTO annonces (title, content, price, category, sous_category, city, postal_code, author_idname, image) VALUES 
+('Jeu de clés à douille', 'Jeu complet de clés à douille pour voiture', 16, 'Voiture', 'Entretien général', 'Dijon', '21000', 'creed', '/home/bmk/Documents/techno4/PretOutils/database/cles.jpg'),
+('Kit de distribution', 'Kit de distribution avec pompe à eau', 48, 'Voiture', 'Réparation de moteur', 'Angers', '49000', 'apollo', '/home/bmk/Documents/techno4/PretOutils/database/distribution.jpg'),
+('Multimètre automobile', 'Multimètre numérique pour voiture', 12, 'Voiture', 'Systèmes électriques', 'Limoges', '87000', 'drago', '/home/bmk/Documents/techno4/PretOutils/database/multimetre.jpg'),
+('Jante alliage', 'Jante en alliage léger 17 pouces', 40, 'Voiture', 'Pneus et roues', 'Clermont-Ferrand', '63000', 'rocky', '/home/bmk/Documents/techno4/PretOutils/database/jante.jpg'),
+('Kit de polissage', 'Kit complet pour polissage carrosserie', 10, 'Voiture', 'Carrosserie et esthétique', 'Brest', '29200', 'rocky', '/home/bmk/Documents/techno4/PretOutils/database/polissage.jpg'),
+('Amortisseurs', 'Amortisseurs avant pour berline', 30, 'Voiture', 'Suspension et direction', 'Le Mans', '72000', 'creed', '/home/bmk/Documents/techno4/PretOutils/database/amortisseurs.jpg'),
+('Silencieux', 'Silencieux en inox pour échappement', 26, 'Voiture', 'Échappement', 'Amiens', '80000', 'apollo', '/home/bmk/Documents/techno4/PretOutils/database/silencieux.jpg');
+
+-- Insertions pour la catégorie 'Électronique'
+INSERT INTO annonces (title, content, price, category, sous_category, city, postal_code, author_idname, image) VALUES 
+('Ordinateur portable', 'Ordinateur portable 15 pouces performant', 200, 'Électronique', 'Informatique', 'Perpignan', '66000', 'drago', '/home/bmk/Documents/techno4/PretOutils/database/ordinateur.jpg'),
+('Antenne parabolique', 'Antenne parabolique de 80 cm', 10, 'Électronique', 'Télécommunications', 'Metz', '57000', 'rocky', '/home/bmk/Documents/techno4/PretOutils/database/antenne.jpg'),
+('Carte électronique', 'Carte mère pour montage électronique', 8, 'Électronique', 'Composants électroniques', 'Besançon', '25000', 'apollo', '/home/bmk/Documents/techno4/PretOutils/database/carte.jpg'),
+('Projecteur LED', 'Projecteur LED 4K UHD', 100, 'Électronique', 'Audiovisuel', 'Orléans', '45000', 'creed', '/home/bmk/Documents/techno4/PretOutils/database/projecteur.jpg'),
+('Caméra de surveillance', 'Caméra IP Wi-Fi pour sécurité intérieure', 14, 'Électronique', 'Sécurité électronique', 'Mulhouse', '68100', 'drago', '/home/bmk/Documents/techno4/PretOutils/database/camera.jpg'),
+('Batterie externe', 'Batterie externe 10000mAh', 5, 'Électronique', 'Énergie et alimentation', 'Rouen', '76000', 'rocky', '/home/bmk/Documents/techno4/PretOutils/database/batterie.jpg');
+
+
+-- Insertions pour la catégorie 'Loisirs'
+INSERT INTO annonces (title, content, price, category, sous_category, city, postal_code, author_idname, image) VALUES 
+('Console de jeux', 'Console de jeux dernière génération', 100, 'Loisirs', 'Jeux vidéo', 'Versailles', '78000', 'drago', '/home/bmk/Documents/techno4/PretOutils/database/console.jpg'),
+('Ballon de football', 'Ballon officiel de la Ligue 1', 4, 'Loisirs', 'Jeux de balle', 'Nancy', '54000', 'rocky', '/home/bmk/Documents/techno4/PretOutils/database/ballon.jpg'),
+('Pétanque', 'Jeu complet de boules de pétanque', 5, 'Loisirs', 'Jeux de boules', 'Avignon', '84000', 'creed', '/home/bmk/Documents/techno4/PretOutils/database/petanque.jpg'),
+('Puzzle 3D', 'Puzzle 3D de la Tour Eiffel', 6, 'Loisirs', 'Jeux de construction', 'Poitiers', '86000', 'apollo', '/home/bmk/Documents/techno4/PretOutils/database/puzzle.jpg'),
+('Piscine gonflable', 'Grande piscine familiale gonflable', 15, 'Loisirs', 'Jeux d eaux', 'Troyes', '10000', 'drago', '/home/bmk/Documents/techno4/PretOutils/database/piscine.jpg');
+
+-- Insertions pour la catégorie 'Autre'
+INSERT INTO annonces (title, content, price, category, sous_category, city, postal_code, author_idname, image) VALUES 
+('Décorations de Noël', 'Lot complet de décorations pour Noël', 4, 'Autre', 'Articles saisonniers', 'Ajaccio', '20000', 'rocky', '/home/bmk/Documents/techno4/PretOutils/database/decorations.jpg'),
+('Diable de déménagement', 'Diable robuste pour charges lourdes', 10, 'Autre', 'Équipement de déménagement', 'Béziers', '34500', 'creed', '/home/bmk/Documents/techno4/PretOutils/database/diable.jpg'),
+('Kit pour mariage', 'Kit de décoration complet pour mariage', 15, 'Autre', 'Matériel de fête et événementiel', 'La Rochelle', '17000', 'apollo', '/home/bmk/Documents/techno4/PretOutils/database/mariage.jpg'),
+('Pièce de collection', 'Pièce de monnaie rare de 1789', 20, 'Autre', 'Articles de collection et hobbies rares', 'Cherbourg', '50100', 'drago', '/home/bmk/Documents/techno4/PretOutils/database/piece.jpg'),
+('Niche pour chien', 'Niche isolée pour grand chien', 12, 'Autre', 'Équipements pour animaux de compagnie', 'Grenoble', '38000', 'rocky', '/home/bmk/Documents/techno4/PretOutils/database/niche.jpg'),
+('Kit éducatif', 'Kit de science pour enfants', 8, 'Autre', 'Matériel éducatif et didactique', 'Bourges', '18000', 'creed', '/home/bmk/Documents/techno4/PretOutils/database/kit.jpg'),
+('Service de jardinage', 'Service de jardinage à domicile', 12, 'Autre', 'Services divers', 'Le Puy-en-Velay', '43000', 'apollo', '/home/bmk/Documents/techno4/PretOutils/database/jardinage.jpg');
+
--- server.js
+++ server.js
@@ -1,104 +1,261 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mysql = require('mysql');
-const cors = require('cors');
-require('dotenv').config(); // Charge les variables d'environnement
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+const cors = require("cors");
+require("dotenv").config(); // Charge les variables d'environnement
+
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function(req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.originalname.split('.').pop());
+    }
+});
+
+const upload = multer({ storage: storage });
+
 
 const app = express();
 app.use(bodyParser.json());
 
 // Configuration de CORS pour autoriser les requêtes de votre application React
-app.use(cors({
-  origin: 'http://localhost:3000' // Autorise les requêtes CORS uniquement pour ce domaine
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Autorise les requêtes CORS uniquement pour ce domaine
+  })
+);
 
 // Connexion à la base de données MySQL
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'root',
-  database: process.env.DB_DATABASE || 'outils_db'
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "root",
+  database: process.env.DB_DATABASE || "outils_db",
 });
 
-db.connect(err => {
+db.connect((err) => {
   if (err) {
-    console.error('Erreur de connexion : ' + err.stack);
+    console.error("Erreur de connexion : " + err.stack);
     return;
   }
-  console.log('Connecté à la base de données MySQL!');
+  console.log("Connecté à la base de données MySQL!");
+});
+
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  if (req.file) {
+      res.json({
+          message: 'Fichier téléchargé avec succès',
+          filename: req.file.filename
+      });
+  } else {
+      res.status(400).send('Erreur lors du téléversement du fichier');
+  }
 });
 
 // Exemple de route API
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.listen(3001, () => {
-  console.log('Serveur démarré sur http://localhost:3001');
-});
-app.get('/annonces', (req, res) => {
+
+app.get("/annonces", (req, res) => {
   // Ajoutez TO_BASE64 autour de la colonne BLOB
-  db.query('SELECT id, title, content, price, category, city, postal_code, author_idname, published_date, TO_BASE64(image) AS imageBase64 FROM annonces', (err, results) => {
+  db.query(
+    "SELECT id, title, content, price, category, city, postal_code, author_idname, published_date, TO_BASE64(image) AS imageBase64 FROM annonces",
+    (err, results) => {
+      if (err) {
+        res.status(500).send("Erreur lors de la récupération des annonces");
+        console.error(err);
+      } else {
+        // Traiter les résultats pour inclure le préfixe nécessaire pour l'interprétation correcte des données en tant qu'images
+        results.forEach((result) => {
+          if (result.imageBase64) {
+            result.image = `data:image/jpeg;base64,${result.imageBase64}`;
+          }
+        });
+        res.json(results);
+      }
+    }
+  );
+});
+
+// Route pour la connexion
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  // Assumant que 'username' correspond à 'idname' dans votre table 'users'
+  db.query(
+    "SELECT idname, password FROM users WHERE idname = ?",
+    [username],
+    (error, results) => {
+      if (error) {
+        console.error(
+          "Erreur lors de la connexion à la base de données :",
+          error
+        );
+        return res.status(500).send({
+          message: "Erreur lors de la connexion à la base de données",
+          success: false,
+        });
+      }
+      if (results.length > 0) {
+        const user = results[0];
+        if (password === user.password) {
+          // Comparaison simpliste, envisagez d'utiliser bcrypt dans un environnement de production pour plus de sécurité
+          res.status(200).send({
+            success: true,
+            message: "Connexion réussie",
+            userId: user.idname,
+          });
+        } else {
+          res.status(401).send({
+            success: false,
+            message: "Identifiant ou mot de passe incorrect",
+          });
+        }
+      } else {
+        res
+          .status(404)
+          .send({ success: false, message: "Identifiants incorrects" });
+      }
+    }
+  );
+});
+
+// Route pour obtenir les informations de l'utilisateur
+app.get("/user/:id", (req, res) => {
+  const userId = req.params.id;
+  const query =
+    "SELECT idname, nom , prenom, email FROM users WHERE idname = ?";
+  db.query(query, [userId], (err, result) => {
     if (err) {
-      res.status(500).send('Erreur lors de la récupération des annonces');
+      res
+        .status(500)
+        .send(
+          "Erreur lors de la récupération des informations de l’utilisateur"
+        );
       console.error(err);
     } else {
-      // Traiter les résultats pour inclure le préfixe nécessaire pour l'interprétation correcte des données en tant qu'images
-      results.forEach(result => {
+      res.json(result);
+    }
+    console.log(result);
+  });
+});
+
+// Route pour obtenir les annonces de l'utilisateur
+app.get("/user/:id/annonces", (req, res) => {
+  const userId = req.params.id;
+  const query =
+    "SELECT id, title, content, price, category, city, postal_code, author_idname, published_date, image FROM annonces WHERE author_idname = ?";
+  db.query(query, [userId], (err, results) => {
+    if (err) {
+      res
+        .status(500)
+        .send("Erreur lors de la récupération des annonces de l’utilisateur");
+      console.error(err);
+    } else {
+      results.forEach((result) => {
         if (result.imageBase64) {
           result.image = `data:image/jpeg;base64,${result.imageBase64}`;
         }
       });
       res.json(results);
+      console.log("Requête pour les annonces de l'utilisateur:", userId);
+      console.log("Résultats:", results);
     }
   });
 });
 
-const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });  // Stocke les fichiers dans le dossier 'uploads'
-
-// Route pour l'upload d'images
-app.post('/upload', upload.single('image'), (req, res) => {
-  const file = req.file;
-  if (!file) {
-    return res.status(400).send({ message: 'Veuillez uploader un fichier.' });
-  }
-
-  const imagePath = file.path;  // Chemin vers l'image sauvegardée
-
-  // Optionnel : Insérer le chemin de l'image dans la base de données
-  const query = "INSERT INTO annonces (image_path) VALUES (?)";
-  db.query(query, [imagePath], (err, result) => {
+// Route pour obtenir tous les messages d'une conversation spécifique
+app.get("/messages/conversation/:conversationId", (req, res) => {
+  const conversationId = req.params.conversationId;
+  const query = "SELECT * FROM messages WHERE conversation_id = ?";
+  db.query(query, [conversationId], (err, results) => {
     if (err) {
-      console.error('Erreur lors de l’insertion du chemin de l’image dans la base de données', err);
-      return res.status(500).send({ message: 'Erreur lors de l’enregistrement du fichier.' });
+      res
+        .status(500)
+        .send("Erreur lors de la récupération des messages de la conversation");
+      console.error(err);
+    } else {
+      res.json(results);
     }
-    res.send({ message: 'Fichier uploadé avec succès.', path: imagePath });
+  });
+});
+
+app.get("/conversations/user/:userId", (req, res) => {
+  const userId = req.params.userId;
+  const query = "SELECT * FROM conversations WHERE creator_idname = ?";
+  db.query(query, [userId], (err, results) => {
+    if (err) {
+      res.status(500).send("Erreur lors de la récupération des conversations");
+      console.error(err);
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+// Route pour effectuer une recherche
+app.get("/recherche/:term", (req, res) => {
+  const term = req.params.term;
+  const query = `SELECT * FROM annonces WHERE title LIKE '%${term}%' OR category LIKE '%${term}%' OR content LIKE '%${term}%'`;
+  db.query(query, (err, results) => {
+    if (err) {
+      res.status(500).send("Erreur lors de la recherche");
+      console.error(err);
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+app.post('/ajouterAnnonce', upload.single('image'), (req, res) => {
+  const { title, content, price, category, sousCategory, city, postalCode } = req.body;
+  const image = req.file ? req.file.path : null;
+  const author_idname = req.body.author_idname; // Supposition sur l'origine de cette donnée
+
+  const insertQuery = 'INSERT INTO annonces (title, content, price, category, sous_category, author_idname, city, postal_code, image) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+  db.query(insertQuery, [title, content, price, category, sousCategory, author_idname, city, postalCode, image], (err, result) => {
+      if (err) {
+          console.error(err);
+          return res.status(500).send("Erreur lors de l'ajout de l'annonce");
+      }
+      res.status(201).send("Annonce ajoutée avec succès");
   });
 });
 
 
-// Route pour la connexion
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-  db.query('SELECT idname, password AS hashedPassword FROM users WHERE idname = ?', [username], async (error, results) => {
-      if (error) {
-          return res.status(500).send('Erreur lors de la connexion à la base de données');
-      }
-      if (results.length > 0) {
-          const comparisonResult = password==results[0].hashedPassword ;
-          console.log('Résultat de la comparaison :', comparisonResult);
-          console.log(password);
-          console.log(results[0].hashedPassword);
-          console.log(results[0].idname);
-          console.log(username);
-          if (comparisonResult) {
-              res.send('Connexion réussie');
-          } else {
-              res.send('Identifiants incorrects');
-          }
+app.get('/categorie/:category/:sousCategory', (req, res) => {
+  const category = req.params.category;
+  const sousCategory = req.params.sousCategory;
+  console.log('Category:', category);
+  console.log('Sous-category:', sousCategory);
+
+  // Updated query to include filtering by sousCategory as well
+  const query = 'SELECT * FROM annonces WHERE category = ? AND sous_category = ?';
+
+  // Execute the query with both category and sousCategory as parameters
+  db.query(query, [category, sousCategory], (err, results) => {
+      if (err) {
+          res.status(500).send("Erreur lors de la récupération des annonces");
+          console.error(err);
       } else {
-          res.send('Identifiants incorrects');
+          res.json(results);
+          console.log('Annonces récupérées:', results);
       }
   });
+});
+
+
+
+
+
+
+
+
+app.listen(3001, () => {
+  console.log("Serveur démarré sur http://localhost:3001");
 });
